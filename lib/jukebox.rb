1751e34d1c861e0b15fed1728f9a0bf62cf3105e
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-13 23:19:59 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -93,6 +93,12 @@ def run(songs)
  print("Please enter a command:")
  user_input = gets.chomp
 
+ if user_input == "exit"
+
+   exit_jukebox
+
+ end
+
    while user_input != "exit" do
      if user_input == "help"
        help
@@ -111,11 +117,7 @@ def run(songs)
      user_input = gets.strip
    end
 
-   if user_input == "exit"
-
-     exit_jukebox
-
-   end
+   
 
 
 
